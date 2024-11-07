77032aa3ccf4dce5b71e134481bde981465e31eb
kali <kali@kali>
2024-11-07 13:08:21 +0200

    fix

--- config/checkReminders.js
+++ config/checkReminders.js
@@ -77,7 +77,7 @@ async function checkDegreeDeadlines(audiances, degre, aboutissement, avocatId, a
       if (aboutissement?.dateInformation) {
         const dateInformation = Math.floor((aboutissement.dateInformation.getTime() - Date.now()) / (1000 * 60 * 60 * 24)) + 1;
 
-        if (dateInformation === 18) {
+        if (dateInformation >=1 && dateInformation <=18 ) {
           const existingDelai = await Delai.findOne({ avocatId, affaireId, type: 'appel', category: 'information', clientId: client });
           if (!existingDelai) {
             await Delai.create({ avocatId, affaireId, type: 'appel', category: 'information', daysRemaining: 0, clientId: client });
